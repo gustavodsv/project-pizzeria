2fc597cce26d06f04a9c33351b761561de738ee8
Gustavo <gustavo-dev@hotmail.com>
2021-01-12 11:36:49 -0300

    adicionando keys e descrições(nome, descrição, imagem) das pizzas

--- script.js
+++ script.js
@@ -6,27 +6,33 @@ const qSA = (el)=>document.querySelectorAll(el);
 pizzaJson.map((item, index)=>{
     let pizzaItem = qS('.models .pizza-item').cloneNode(true);
 
+    pizzaItem.setAttribute('data-key', index); // gerando chave-pizzas
     pizzaItem.querySelector('.pizza-item--img img').src = item.img
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2).replace('.',',')}`
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
+    pizzaItem.querySelector('a').addEventListener('click', (e)=>{
+        e.preventDefault();
+        // adicionando chave às pizzas
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        
+        // adicionando informações
+        qS('.pizzaBig img').src = pizzaJson[key].img;
+        qS('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        qS('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        
+        // adicionando animação
+        qS('.pizzaWindowArea').style.opacity = 0;
+        qS('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(()=>{
+        qS('.pizzaWindowArea').style.opacity = 1;
+        },200); // 1s = 1/5
+        
+    });
     
     qS('.pizza-area').append(pizzaItem); 
 });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-// cloneNode(true) = clona itens selecionados
-// append(itemNome) = adiciona
-// toFixed(2) = 2 itens apos a virgula
